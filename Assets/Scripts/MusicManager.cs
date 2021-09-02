a06afde7b4332e1c5d080b3b5092e2846d175924
tsnr80 <tim.schneider@mni.thm.de>
2021-09-02 23:16:29 +0200

    Created Music Manager

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : SingletonMonoBehaviour<MusicManager> {
+
+    private void Awake() {
+
+        base.Awake();
+    }
+}

1f71c1fcbd3c377a06c46fa3b7ced8d5d6212b48
tsnr80 <tim.schneider@mni.thm.de>
2021-09-03 13:02:26 +0200

    Added background music for the dungeon and the village(gray/colourfull).

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -4,8 +4,48 @@ using UnityEngine;
 
 public class MusicManager : SingletonMonoBehaviour<MusicManager> {
 
+    private static AudioClip forestDungeonTheme;
+    private static AudioClip villageTheme;
+    private static AudioClip grayVillageTheme;
+
+    private static AudioSource audioSource;
+
     private void Awake() {
 
         base.Awake();
+
+        forestDungeonTheme = Resources.Load<AudioClip>(Music.ForestDungeonTheme.ToString());
+        villageTheme = Resources.Load<AudioClip>(Music.VillageTheme.ToString());
+        grayVillageTheme = Resources.Load<AudioClip>(Music.GrayVillageTheme.ToString());
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = true;
     }
+
+    private void Start() {
+
+        // Only for testing purpose
+
+        PlayMusic(Music.ForestDungeonTheme);
+    }
+
+    public static void PlayMusic(Music music) {
+
+        switch (music) {
+
+            case (Music.ForestDungeonTheme):
+                audioSource.Stop();
+                audioSource.PlayOneShot(forestDungeonTheme);
+                break;
+            case (Music.VillageTheme):
+                audioSource.Stop();
+                audioSource.PlayOneShot(villageTheme);
+                break;
+                case (Music.GrayVillageTheme):
+                audioSource.Stop();
+                audioSource.PlayOneShot(grayVillageTheme);
+                break;
+        }
+    }
+
 }
